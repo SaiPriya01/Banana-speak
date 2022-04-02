ec15016ba8e45f041d35189346be8fd306b5aebe
SaiPriya01 <101970704+SaiPriya01@users.noreply.github.com>
2022-04-02 15:46:21 +0530

    added js code

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-var btnTranslate=document.querySelector("#btn-translate");
-var txtInput=document.querySelector("#txt-input");
-console.log(txtInput);
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputDiv=document.querySelector("#output");
+// outputDiv.innerText="Sai Priya"
+// console.log(txtInput);
 // console.log(btnTranslate);
-function clickEventHandler(){
-    console.log("Clicked");
-    console.log("input",txtInput.value);
+var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+function getTranslationURL(text){
+    return serverURL+"? text=" +text
+}
+function clickEventHandler() {
+    // console.log("Clicked");
+    // console.log("input", txtInput.value);
+    var inputTxt=txtInput;
+    fetch(getTranslationURL(inputTxt))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText;
+    })
 };
 // clickEventHandler is Callback function
-btnTranslate.addEventListener("click", clickEventHandler())
-
-
+btnTranslate.addEventListener("click", clickEventHandler);
