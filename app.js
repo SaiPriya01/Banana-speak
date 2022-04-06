be0c7a42c07552f66f273be0b566e892ed6fb76b
SaiPriya01 <101970704+SaiPriya01@users.noreply.github.com>
2022-04-06 13:23:23 +0530

    feat: added web speech API

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector(".output");
-var action = document.getElementById("action");
+// var action = document.getElementById("action");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
 
-function runSpeechRecognition(){
-    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-    var recognition = new SpeechRecognition();
-    recognition.onstart = function() {
-                    action.innerHTML = "<small>listening, please speak...</small>";
-                };
-    recognition.onspeechend = function() {
-                    action.innerHTML = "<small>stopped listening, hope you are done...</small>";
-                    recognition.stop();
+function textToAudio(msg) {
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+
+    speech.text = msg;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    window.speechSynthesis.speak(speech);
+}
+btnTranslate.addEventListener("click", clickHandler);
+function clickHandler() {
+    recognition.start();
+    console.log('start speech');
+    recognition.speechend = () => {
+        recognition.stop();
+        console.lof('speech end')
     }
-    recognition.onresult = function(event) {
-        var transcript = event.results[0][0].transcript;
-        txtInput.innerHTML = transcript;
-        // txtInput.classList.remove("hide");
-    };
-  
-     // start recognition
-     recognition.start();
+    recognition.onresult = function (event) {
+        var transcriptInput = event.results[0][0].transcript;
+        txtInput.innerText = transcriptInput;
+        console.log(event.results[0][0]);
+        console.log(txtInput.value)
+function getTranslation(Txt) {
+    return serverURL + "?text=" + Txt;
 }
 
-
-// function getTranslation(Txt) {
-//     return serverURL + "?text=" + Txt;
-// }
-// function clickHandler() {
-//     var inputTxt = txtInput.value;
-//     console.log(getTranslation(inputTxt))
-//     fetch(getTranslation(inputTxt))
-//         .then(response => response.json())
-//         .then(json => {
-//             outputDiv.innerText = json.contents.translated;
-//         })
-// }
-// btnTranslate.addEventListener("click", clickHandler);
+var translatedText = () => {
+    var Input = transcriptInput;
+    fetch(getTranslation(Input))
+        .then(response => response.json())
+        .then(json => {
+            let outputText=json.contents.translated;
+            outputDiv.innerText = outputText;
+            textToAudio(outputText);
+        })
+}
+translatedText();
+}
+}
