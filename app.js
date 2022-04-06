54c7be604ae97ed3a576d185e24a48a1112e2d81
SaiPriya01 <101970704+SaiPriya01@users.noreply.github.com>
2022-04-06 11:25:32 +0530

    practice audio to text js

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector(".output");
-
+var action = document.getElementById("action");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslation(Txt) {
-    return serverURL + "?text=" + Txt;
+function runSpeechRecognition(){
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
+    recognition.onstart = function() {
+                    action.innerHTML = "<small>listening, please speak...</small>";
+                };
+    recognition.onspeechend = function() {
+                    action.innerHTML = "<small>stopped listening, hope you are done...</small>";
+                    recognition.stop();
+    }
+    recognition.onresult = function(event) {
+        var transcript = event.results[0][0].transcript;
+        txtInput.innerHTML = transcript;
+        // txtInput.classList.remove("hide");
+    };
+  
+     // start recognition
+     recognition.start();
 }
-function clickHandler() {
-    var inputTxt = txtInput.value;
-    console.log(getTranslation(inputTxt))
-    fetch(getTranslation(inputTxt))
-        .then(response => response.json())
-        .then(json => {
-            outputDiv.innerText = json.contents.translated;
-        })
-}
-btnTranslate.addEventListener("click", clickHandler);
+
+
+// function getTranslation(Txt) {
+//     return serverURL + "?text=" + Txt;
+// }
+// function clickHandler() {
+//     var inputTxt = txtInput.value;
+//     console.log(getTranslation(inputTxt))
+//     fetch(getTranslation(inputTxt))
+//         .then(response => response.json())
+//         .then(json => {
+//             outputDiv.innerText = json.contents.translated;
+//         })
+// }
+// btnTranslate.addEventListener("click", clickHandler);
